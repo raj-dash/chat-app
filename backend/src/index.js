2fa1283d0c96126516fc9e59b5ec27be98503694
raj-dash <rajaryanrad@gmail.com>
2025-05-18 22:09:15 +0530

    set up mongodb connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
+
 import authRoutes from "./routes/auth.route.js"
+import {connectDB} from "./lib/db.js"
 
 const app = express();
 
@@ -12,4 +14,5 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
     console.log("Server is running on PORT:" + PORT);
+    connectDB()
 })
