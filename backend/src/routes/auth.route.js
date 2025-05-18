df4a8a03bee0d562d88e9a19f64bbb5cdbfd7a8a
raj-dash <rajaryanrad@gmail.com>
2025-05-18 21:23:41 +0530

    set up controllers for login, signup and logout

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,16 @@
 import express from "express";
+import { signup, logout, signup } from "../controllers/auth.controller.js"
 
 const router = express.Router();
+
+router.post("/signup", )
+
+router.post("/login", (req, res) => {
+    res.send("login route");
+})
+
+router.get("/logout", (req, res) => {
+    res.send("logout route");
+})
 
 export default router;
