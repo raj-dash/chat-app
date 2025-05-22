4fb1ea1de13b89463b61b6feaf20aa4b0886c699
raj-dash <rajaryanrad@gmail.com>
2025-05-22 14:24:35 +0530

    Created check end point to authenticate on reload

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -109,4 +109,13 @@ export const updateProfile = async (req, res) => {
         console.log("Error in updateProfile: ", error.message)
         res.status(500).json({ message: "Internal Server Error" })
     }
-}
+};
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user)
+    } catch (error) {
+        console.log("Error in checkAuth controller: ", error.message)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, logout, login } from "../controllers/auth.controller.js"
+import { signup, logout, login, checkAuth, updateProfile } from "../controllers/auth.controller.js"
 import { protectRoute } from "../middleware/auth.middleware.js"
 
 const router = express.Router();
@@ -11,5 +11,7 @@ router.post("/login", login)
 router.post("/logout", logout)
 
 router.put("/update-profile", protectRoute, updateProfile)
+
+router.get("/check", protectRoute, checkAuth)
 
 export default router;
