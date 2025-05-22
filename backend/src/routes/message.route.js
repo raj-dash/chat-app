0e4e9b283ca357cdc4507a0fbac9e64ff60f849b
raj-dash <rajaryanrad@gmail.com>
2025-05-22 16:17:33 +0530

    Added get messages function

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware";
-import { getUsersForSidebar } from "../controllers/message.controller";
+import {
+  getMessages,
+  getUsersForSidebar,
+} from "../controllers/message.controller";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
