37b2b42951792dd07bdbd1e90736ed54f2259517
raj-dash <rajaryanrad@gmail.com>
2025-05-22 15:53:07 +0530

    Reformatted code using prettier

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
-import express from "express"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.route.js"
-import {connectDB} from "./lib/db.js"
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
@@ -15,8 +15,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-
 app.listen(PORT, () => {
-    console.log("Server is running on PORT:" + PORT);
-    connectDB()
-})
+  console.log("Server is running on PORT:" + PORT);
+  connectDB();
+});
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,23 @@
 import express from "express";
-import { signup, logout, login, checkAuth, updateProfile } from "../controllers/auth.controller.js"
-import { protectRoute } from "../middleware/auth.middleware.js"
+import {
+  signup,
+  logout,
+  login,
+  checkAuth,
+  updateProfile,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/signup", signup)
+router.post("/signup", signup);
 
-router.post("/login", login)
+router.post("/login", login);
 
-router.post("/logout", logout)
+router.post("/logout", logout);
 
-router.put("/update-profile", protectRoute, updateProfile)
+router.put("/update-profile", protectRoute, updateProfile);
 
-router.get("/check", protectRoute, checkAuth)
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
