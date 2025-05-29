efec9d4a8995d1419728cce19acf66e3d218d818
raj-dash <rajaryanrad@gmail.com>
2025-05-29 12:21:18 +0530

    added function to logout

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -35,4 +35,14 @@ export const useAuthStore = create((set) => ({
       set({ isSigningUp: false });
     }
   },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged Out Successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
 }));
